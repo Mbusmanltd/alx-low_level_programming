6c9d73c15e6072b3173617ad6c78dd70fd1e0f36
Mbusmanltd <mbusmanltd@gmail.com>
2022-09-08 20:39:50 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	                 puts("\"programming is like building a multilingual puzzle"
+			     ):
+				            return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-script that compiles a C file and creates an executable named cisfun
+Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
