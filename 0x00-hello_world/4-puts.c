f9715ddfcf77b73461732dca0a03fcff082b4d5d
Mbusmanltd <mbusmanltd@gmail.com>
2022-09-08 21:44:13 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
- script that generates the assembly code of a C code and save it in an output file
+ C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
