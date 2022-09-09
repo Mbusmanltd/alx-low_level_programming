ec0f5a9521661f5d7fdd0fea4fced423afe5c384
Mbusmanltd <mbusmanltd@gmail.com>
2022-09-09 11:41:22 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
