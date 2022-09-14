4b8ed4c9f4b60b44dfc630dab45370f276c05ed1
mbusmanltd <mbusmanldt@gmail.com>
2022-09-13 22:46:12 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times Alphabet in lowercase
+ *
+ * Return Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char r;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r);
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
