5a899d0ed2cddd94e9748c748de3713c742043bf
mbusmanltd <mbusmanldt@gmail.com>
2022-09-16 05:25:04 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - Drawing a triangle using C
+ *
+ * @size: size of the triangle
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_triangle(int size)
+{
+	int hght, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hght = 1; hght <= size; ++hght)
+		{
+			for (base = 1; base <= size; ++base)
+			{
+				if ((hght + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
