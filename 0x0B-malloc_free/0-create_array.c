76b91944a4ad97697ec6de15f9857b094d30dbb8
Mbusmanltd <mbusmanltd@gmail.com>
2022-10-05 07:30:18 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,66 +1,40 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include "main.h"
 
-
-
 /**
+ * *create_array - creates an array of chars
+ * and initializes it with a specific char~
+ * @size: size of the array to create
+ * @c: char to initialize the array c
  *
- *  * *create_array - creates an array of chars,
- *
- *   * and initializes it with a specific char~
- *
- *    * @size: size of the array to create
- *
- *     * @c: char to initialize the array c
- *
- *      *
- *
- *       * Return: pointer to the array (Success), NULL (Error)
- *
- *        */
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 
 char *create_array(unsigned int size, char c)
 
 {
 
-		char *p;
+        char *p;
+	unsigned int i = 0;
 
-			unsigned int i = 0;
+	if (size == 0)
+	              return (NULL);
 
+        p = (char *) malloc(sizeof(char) * size);
 
+       if (p == NULL)
+	             return (0);
 
-				if (size == 0)
+       while (i < size)
+       {
 
-							return (NULL);
+									        *(p + i) = c;
+										i++;
 
-
-
-					p = (char *) malloc(sizeof(char) * size);
-
-
-
-						if (p == NULL)
-
-									return (0);
-
+									}
 
 
-							while (i < size)
-
-									{
-
-												*(p + i) = c;
-
-														i++;
-
-															}
-
-
-
-								*(p + i) = '\0';
-
-
+       *(p + i) = '\0';
 
 									return (p);
 
