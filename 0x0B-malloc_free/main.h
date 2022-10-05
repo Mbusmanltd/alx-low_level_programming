b95e59cb17085bd43d6b9d1c6801d2e35f5a89cb
Mbusmanltd <mbusmanltd@gmail.com>
2022-10-05 07:11:18 -0700

    task1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,23 +1,12 @@
-#ifndef FILE_MAIN
+#include <stdlib.h> 
 
-#define FILE_MAIN
-
-
-
+int _putchar(char);
 char *create_array(unsigned int size, char c);
-
 char *_strdup(char *str);
-
 char *str_concat(char *s1, char *s2);
-
 int **alloc_grid(int width, int height);
-
 void free_grid(int **grid, int height);
-
 char *argstostr(int ac, char **av);
-
 char **strtow(char *str);
 
-
-
-#endif
+#endif /* MAIN_H */
