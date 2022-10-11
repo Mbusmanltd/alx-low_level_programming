d5d6f2319ee2b56984c16cdaaf83eeb6341923d9
mbusmanltd <mbusmanldt@gmail.com>
2022-10-11 06:05:57 -0700

    task2

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 void dog_t(struct dog)	
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
